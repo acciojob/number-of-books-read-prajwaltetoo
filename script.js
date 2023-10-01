27c58d4b4d35b937414026fcc7f82425adad6306
prajwaltetoo <119670042+prajwaltetoo@users.noreply.github.com>
2023-10-01 16:26:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
-	 let count = 0;
+	 let count=0;
 	library.forEach((i)=>{
 		if(i.readingStatus===true)
 		{
@@ -26,7 +26,7 @@ const numberOfBooksRead = () => {
 		}
 	})
 	return count;
-	
+};
 
 // Do not change the code below
 
